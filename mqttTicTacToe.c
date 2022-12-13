3465655aa61e1a2aac65731743628685fdbfba1b
AllenChiangCPP <allenchiang@cpp.edu>
2022-12-13 01:28:13 -0800

    first commit. Added plans for coding TicTacToe

---
+++ mqttTicTacToe.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <MQTTClient.h>
+
+
+//copy code from old ticTacToe assignment
+//modify code so 
+int main()
+{
+    printf("TEST");
+    return 0;
+}
