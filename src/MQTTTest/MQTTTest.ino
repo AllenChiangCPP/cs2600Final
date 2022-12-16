368003645a7e6ed45a28e8fb17931831fb3b57d6
AllenChiangCPP <allenchiang@cpp.edu>
2022-12-15 23:02:52 -0800

    Added arduino esp32 files to be tracked. Added plans in arduino code to add buttons and use lcd screen for tic tac toe game due to super starter kit not having enough parts.

---
+++ src/MQTTTest/MQTTTest.ino
@@ -0,0 +1,80 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
+#define SDA 13
+#define SCL 14
+
+// WiFi
+const char *ssid = "@_@ (2)"; // Enter your WiFi name
+const char *password = "BudgetedBad";  // Enter WiFi password
+
+// MQTT Broker
+const char *mqtt_broker = "broker.emqx.io";
+const char *topic = "testConnect";
+const char *mqtt_username = "emqx";
+const char *mqtt_password = "public";
+const int mqtt_port = 1883;
+
+//lcd screen, will display adata for the tic tac toe game such as what boxes available
+WiFiClient espClient;
+PubSubClient client(espClient);
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+//add button data, cycles through numbers 0-9 for picking boxes in the tic tac toe game and picking options
+
+void setup() {
+ //setup lcd screen
+ Wire.begin(SDA, SCL);
+ lcd.init();
+ lcd.backlight();
+ lcd.setCursor(0,0);
+ lcd.print("START? (y=1/n=2)");
+ // Set software serial baud to 115200;
+ Serial.begin(115200);
+ // connecting to a WiFi network
+ WiFi.begin(ssid, password);
+ while (WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.println("Connecting to WiFi..");
+ }
+ Serial.println("Connected to the WiFi network");
+ //connecting to a mqtt broker
+ client.setServer(mqtt_broker, mqtt_port);
+ client.setCallback(callback);
+ while (!client.connected()) {
+     String client_id = "esp32-client-";
+     client_id += String(WiFi.macAddress());
+     Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+     if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+         Serial.println("Public emqx mqtt broker connected");
+     } else {
+         Serial.print("failed with state ");
+         Serial.print(client.state());
+         delay(2000);
+     }
+ }
+ // publish and subscribe
+ client.publish(topic, "Hi EMQX I'm ESP32");
+ client.subscribe(topic);
+
+
+}
+
+void callback(char *topic, byte *payload, unsigned int length) {
+ Serial.print("Message arrived in topic: ");
+ Serial.println(topic);
+ Serial.print("Message:");
+ for (int i = 0; i < length; i++) {
+     Serial.print((char) payload[i]);
+ }
+ Serial.println();
+ Serial.println("-----------------------");
+}
+
+void loop() 
+{
+  client.loop();
+
+}
