bece55f5228d691067d7544bea41b7875f4e580a
AllenChiangCPP <allenchiang@cpp.edu>
2022-12-16 21:50:33 -0800

    Added auto.sh file for daemon program.

---
+++ src/auto.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+HOST="broker.emqx.io"
+TOPIC="testConnect"
--- src/mqttTicTacToe.c
+++ src/mqttTicTacToe.c
@@ -14,6 +14,24 @@
 //change so uses 1d array, 0-8 to build and compare square
 //figure out how to take input from esp32
 
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message)
+{
+    int i;
+    char* payloadptr;
+    printf("Message arrived\n");
+    printf("     topic: %s\n", topicName);
+    printf("   message: ");
+    payloadptr = message->payload;
+    for(i=0; i<message->payloadlen; i++)
+    {
+        putchar(*payloadptr++);
+    }
+    putchar('\n');
+    MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName);
+    return 1;
+}
+
 int main(int argc, char* argv[])
 {
     MQTTClient client;
@@ -21,6 +39,7 @@ int main(int argc, char* argv[])
     MQTTClient_message pubmsg = MQTTClient_message_initializer;
     MQTTClient_deliveryToken token;
     int rc;
+    int ch;
 
     MQTTClient_create(&client, ADDRESS, CLIENTID,
         MQTTCLIENT_PERSISTENCE_NONE, NULL);
@@ -46,6 +65,14 @@ int main(int argc, char* argv[])
             (int)(TIMEOUT/1000), PAYLOAD, TOPIC, CLIENTID);
     rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
     printf("Message with delivery token %d delivered\n", token);
+
+    MQTTClient_subscribe(client, TOPIC, QOS);
+    do
+    {
+        ch  = getchar();
+        printf("test: %d",ch);
+    } while (ch != 'Q' && ch != 'q');
+    
   
     // Disconnect
     MQTTClient_disconnect(client, 10000);
